3c70f729e36d4b2e30ff547e816c3418b0f73b77
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-22 21:38:20 +0530

    [Sai] Compared lengths of two lines

--- Line_compare.java
+++ Line_compare.java
@@ -7,18 +7,42 @@ public class {
 
 		Scanner fetch = new Scanner(System.in);
 
-                System.out.println("Give me the First X Co-ordinate");
+                System.out.println("Give me the First X Co-ordinate of First Line");
                 int x_one = fetch.nextInt();
-                System.out.println("Give me the First Y Co-ordinate");
+                System.out.println("Give me the First Y Co-ordinate of First Line");
                 int y_one = fetch.nextInt();
-                System.out.println("Give me the Second X Co-ordinate");
+                System.out.println("Give me the Second X Co-ordinate of First Line");
                 int x_two = fetch.nextInt();
-                System.out.println("Give me the Second Y Co-ordinate");
+                System.out.println("Give me the Second Y Co-ordinate of First Line");
                 int y_two = fetch.nextInt();
 
-                double line_length = Math.sqrt(( Math.pow((x_two - x_one) , 2 )+ Math.pow((y_two- y_one) ,2 )));
+                double line_one_length = Math.sqrt(( Math.pow((x_two - x_one) , 2 )+ Math.pow((y_two- y_one) ,2 )));
 
-                System.out.println("The length of the line is "+line_length);
+                System.out.println("The length of the line is "+line_one_length);
+
+		System.out.println("Give me the First X Co-ordinate of second Line");
+                int x_two_one = fetch.nextInt();
+                System.out.println("Give me the First Y Co-ordinate of Second Line");
+                int y_two_one = fetch.nextInt();
+                System.out.println("Give me the Second X Co-ordinate of Second Line");
+                int x_two_two = fetch.nextInt();
+                System.out.println("Give me the Second Y Co-ordinate of Second Line");
+                int y_two_two = fetch.nextInt();
+
+                double line_two_length = Math.sqrt(( Math.pow((x_two_two - x_two_one) , 2 )+ Math.pow((y_two_two - y_two_one) ,2 )));
+
+                System.out.println("The length of the line is "+line_two_length);
+
+		if ( line_two_length == line_one_length ) {
+
+			System.out.println("THE LINES ARE EQUAL IN LENGTH");
+		}
+
+		else {
+
+			System.out.println("THE LINES ARE NOT EQUAL IN LENGTH");
+
+		}
 
 
 		}
